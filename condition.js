b4976a5a2e3078a7f04fdcc19ddd7875ffbd9dbd
winshabiha <sabiha3289@gmail.com>
2024-09-18 19:46:23 +0600

    first commit

---
+++ condition.js
@@ -0,0 +1,122 @@
+var num1 = 10;
+var num2 = 10;
+// if(num1 > num2){
+//     console.log('num1 is larger than num2')
+// }
+// else if(num1 < num2){
+//     console.log('num1 is smaller than num2')
+// }
+// else{
+//     console.log( 'num1 and num2 are equal' )
+// }
+
+
+// simple values = 3,-7,2
+var x = 3;
+var y = -7;
+var z = 2;
+// if( x>0 && y>0 && z>0){
+//     console.log('all the elements are bigger than 0')
+// }
+// else if( x<0 && y<0 && z<0){
+//     console.log('zero is the largest among all the numbers')
+// }
+// else if(x<0 && y>0 && z<0){
+//     console.log('among three y is bigger than others')
+// }
+// else if(x>0 && y<0 && z>0){
+//     console.log('only the y is less than zero')
+// }
+// else{
+//     console.log('they are not the real number')
+// }
+
+
+var bangla = 45;
+var english = 60;
+var mathematics = 90;
+// if((bangla >=45 && english >=60 || mathematics >=80)){
+//     console.log('you will allow for the next session')
+// }
+// else{
+//     console.log('you are disqualified')
+// }
+
+
+var bangla = 65;
+var english = 50;
+var mathematics = 75;
+// if((bangla >=45 && english >=60 || mathematics >=80)){
+//     console.log('you will allow for the next session')
+// }
+// else{
+//     console.log('you are disqualified')
+// }
+
+
+var physics = 18;
+var chemistry = 20;
+var biology = 25;
+var english = 10;
+var gk = 8;
+// if(( gk >=8 && english >10 && physics >15) || (biology >=25 && chemistry >20)){
+//     console.log('your name will include in the M3 batch')
+// }
+// else{
+//     console.log('you will remain in the same batch')
+// }
+// i
+
+var OddNumber = 7;
+var EvenNumber = 6;
+var SimpleDigit = 5;
+// if(OddNumber >0 && EvenNumber >OddNumber &&  SimpleDigit > -1){
+//     console.log('agreed to all the condition')
+// }
+// else if( EvenNumber==SimpleDigit || (OddNumber <EvenNumber && SimpleDigit >OddNumber)){
+//     console.log('it is also correct')
+// }
+// else if(OddNumber >5 || EvenNumber>7){
+//     console.log('agreed to one condition')
+// }
+// else{
+//     console.log('do not agree to the conditions')
+// }
+
+var category1 = 12000;
+// if(category1 >=10000){
+    //    Discont 12%
+//     var GetDiscount = category1 * 12/100
+//     console.log(GetDiscount)
+// }
+
+var category2 = 5000;
+// if( category2 >=7000){
+//     var GetDiscount = category2 * 7/100
+//     console.log('applicable')
+// }
+// else{
+//     console.log('not applicable')
+// }
+
+var category3 = 3000;
+// if(category3 >=2000){
+//     var GetDiscont = category3 * 5/100
+//     // console.log(GetDiscont)
+//     var priceAfterDiscount = category3 - GetDiscont
+//     console.log(priceAfterDiscount)
+// }
+
+// ternary operator
+// category3 === 3000? 'He or she will get 5% discound': 'no discount'
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,46 @@
+// IfElse
+
+if(5>7){
+//     console.log('if block')
+// }
+// else{
+//     console.log('if not block')
+}
+
+var tiger = 'lion'; 
+// if(tiger === 'lion'){
+//     console.log('they are brave')
+// }
+// else if(cat ==="dog"){
+//     console.log('they are small in size')
+// }
+// else{
+//     console.log('they are not equal')
+// }
+
+if(false){
+//     console.log('if block')
+// }
+// else if(true){
+//     console.log('if not block')
+// }
+// else{
+//     console.log('unblock')
+}
+
+
+if(false){
+//     console.log('if block')
+// }
+// else if(true){
+//     console.log('if not block')
+// }
+// else if(true){
+//     console.log('if block not at all')
+// }
+// else{
+//     console.log('unblock')
+}
+
+
+
